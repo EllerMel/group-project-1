30d43be2744820833446a1132ad5512d3afa6f67
Unbroken Technology <s.albert.staton@gmail.com>
2019-03-28 10:56:12 -0400

    fixed merge

--- roadrunner/src/views/confirmation.vue
+++ roadrunner/src/views/confirmation.vue
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 <template>
   <div class="confirmation">
     <h1>This is Shawn's confirmation page</h1>
   </div>
 </template>
-=======
+
 
 <template>
   <div class="confirmation">
