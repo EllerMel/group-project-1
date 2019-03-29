e4ce7d62ee78b584a8ad8a5f2890edc0d1880a48
M Eller <msmaedev@gmail.com>
2019-03-28 20:39:38 -0400

    P3S2 Admin page set up

--- roadrunner/src/router.js
+++ roadrunner/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Confirmation from './views/confirmation.vue'
+import Admin from './views/Admin'
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin
     },
     {
       path: '/confirmation',
